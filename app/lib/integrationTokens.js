ffa4db8c52c3f8eb132cd87eb2a3313541731d8e
sebastianauls-cmyk <sebastian.auls@gmail.com>
2026-09-01 14:28:43 +0200

    V46 fix ESM integration token re-export path

--- app/lib/integrationTokens.js
+++ app/lib/integrationTokens.js
@@ -1,1 +1,1 @@
-export { openIntegrationToken, sealIntegrationToken } from '../modules/integrations/tokens'
+export { openIntegrationToken, sealIntegrationToken } from '../modules/integrations/tokens.js'
