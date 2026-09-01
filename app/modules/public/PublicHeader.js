3e55c301c745016ecea398d104315d490bacc71f
sebastianauls-cmyk <sebastian.auls@gmail.com>
2026-09-01 23:28:52 +0200

    v46: extract public header module

---
+++ app/modules/public/PublicHeader.js
@@ -0,0 +1,33 @@
+'use client'
+
+import { AppLogo } from '../workspace/AppLogo'
+import { PublicLanguageModules } from './PublicLanguageModules'
+
+export function PublicHeader({
+  t,
+  language,
+  onLanguageChange,
+  outputLanguage,
+  onOutputLanguageChange,
+  onScreenChange,
+  onPlayExplainer
+}){
+  return <header className="publicTop">
+    <div className="wrap nav publicHeader">
+      <div className="brand publicBrand"><AppLogo/><b>AS Gold</b></div>
+      <nav className="publicActions publicNavActions">
+        <a href="#fallarten">{t.nav}</a>
+        <a href="#preise">{t.prices}</a>
+        <button className="secondary" onClick={()=>onScreenChange('register')}>{t.register}</button>
+        <button className="primary" onClick={()=>onScreenChange('login')}>{t.login}</button>
+      </nav>
+      <PublicLanguageModules
+        language={language}
+        onLanguageChange={onLanguageChange}
+        outputLanguage={outputLanguage}
+        onOutputLanguageChange={onOutputLanguageChange}
+        onPlayExplainer={onPlayExplainer}
+      />
+    </div>
+  </header>
+}
