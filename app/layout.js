dcb7aa1e762f0f55a95520353a2eb68506d044fb
sebastianauls-cmyk <sebastian.auls@gmail.com>
2026-09-01 15:43:50 +0200

    V48 hotfix: remove conflicting navigation observers on mobile

--- app/layout.js
+++ app/layout.js
@@ -16,12 +16,8 @@ import { V39CaseTimelineAutoAssessment } from './components/V39CaseTimelineAutoA
 import { V40ProfessionalHandoff } from './components/V40ProfessionalHandoff'
 import { V41CaseConsistency } from './components/V41CaseConsistency'
 import { V42ActionableGaps } from './components/V42ActionableGaps'
-import { V43VisibilityFix } from './components/V43VisibilityFix'
-import { V44LanguageOrder } from './components/V44LanguageOrder'
 import { V45OutputLanguageBridge } from './components/V45OutputLanguageBridge'
-import { V46MobileLanguageAcceptance } from './components/V46MobileLanguageAcceptance'
 import { V47SimplifiedPublicStart } from './components/V47SimplifiedPublicStart'
-import { V48FinalUiGuard } from './components/V48FinalUiGuard'
 
 export const metadata = {
   metadataBase: new URL('https://app-gold-workspace.vercel.app'),
@@ -32,5 +28,5 @@ export const metadata = {
 }
 
 export default function RootLayout({ children }) {
-  return <html lang="de"><body><V45OutputLanguageBridge/><V46MobileLanguageAcceptance/><V47SimplifiedPublicStart/><V38AccessibilityHardening/><V38MobileResilience/><V38IntegrationAvailabilityGuard/><HeroCopyEnhancer/><HeroTitleStabilizer/><V37FirstAction/><ProblemNavigator/><ExplainerVideo/><ProductIntroCompact/><CaseChoiceJumpEnhancer/><V38DeadlineCardEnhancer/><V38AssessmentExplainability/><V38PrimaryNextStep/><V39CaseTimelineAutoAssessment/><V40ProfessionalHandoff/><V41CaseConsistency/><V42ActionableGaps/><V43VisibilityFix/><V44LanguageOrder/><V48FinalUiGuard/>{children}</body></html>
+  return <html lang="de"><body><V45OutputLanguageBridge/><V47SimplifiedPublicStart/><V38AccessibilityHardening/><V38MobileResilience/><V38IntegrationAvailabilityGuard/><HeroCopyEnhancer/><HeroTitleStabilizer/><V37FirstAction/><ProblemNavigator/><ExplainerVideo/><ProductIntroCompact/><CaseChoiceJumpEnhancer/><V38DeadlineCardEnhancer/><V38AssessmentExplainability/><V38PrimaryNextStep/><V39CaseTimelineAutoAssessment/><V40ProfessionalHandoff/><V41CaseConsistency/><V42ActionableGaps/>{children}</body></html>
 }
