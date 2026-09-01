b87623802967ad7923d6a5116b9445d5632088df
sebastianauls-cmyk <sebastian.auls@gmail.com>
2026-09-01 04:31:58 +0200

    V45: mount output-language request bridge

--- app/layout.js
+++ app/layout.js
@@ -18,6 +18,7 @@ import { V41CaseConsistency } from './components/V41CaseConsistency'
 import { V42ActionableGaps } from './components/V42ActionableGaps'
 import { V43VisibilityFix } from './components/V43VisibilityFix'
 import { V44LanguageOrder } from './components/V44LanguageOrder'
+import { V45OutputLanguageBridge } from './components/V45OutputLanguageBridge'
 
 export const metadata = {
   metadataBase: new URL('https://app-gold-workspace.vercel.app'),
@@ -28,5 +29,5 @@ export const metadata = {
 }
 
 export default function RootLayout({ children }) {
-  return <html lang="de"><body><V38AccessibilityHardening/><V38MobileResilience/><V38IntegrationAvailabilityGuard/><HeroCopyEnhancer/><HeroTitleStabilizer/><V37FirstAction/><ProblemNavigator/><ExplainerVideo/><ProductIntroCompact/><CaseChoiceJumpEnhancer/><V38DeadlineCardEnhancer/><V38AssessmentExplainability/><V38PrimaryNextStep/><V39CaseTimelineAutoAssessment/><V40ProfessionalHandoff/><V41CaseConsistency/><V42ActionableGaps/><V43VisibilityFix/><V44LanguageOrder/>{children}</body></html>
+  return <html lang="de"><body><V45OutputLanguageBridge/><V38AccessibilityHardening/><V38MobileResilience/><V38IntegrationAvailabilityGuard/><HeroCopyEnhancer/><HeroTitleStabilizer/><V37FirstAction/><ProblemNavigator/><ExplainerVideo/><ProductIntroCompact/><CaseChoiceJumpEnhancer/><V38DeadlineCardEnhancer/><V38AssessmentExplainability/><V38PrimaryNextStep/><V39CaseTimelineAutoAssessment/><V40ProfessionalHandoff/><V41CaseConsistency/><V42ActionableGaps/><V43VisibilityFix/><V44LanguageOrder/>{children}</body></html>
 }
