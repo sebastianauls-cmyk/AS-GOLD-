b13e7442b6b7b6523e7917318c883a58bdc367da
sebastianauls-cmyk <sebastian.auls@gmail.com>
2026-09-01 02:25:28 +0200

    V38: mount mobile resilience layer

--- app/layout.js
+++ app/layout.js
@@ -9,6 +9,7 @@ import { CaseChoiceJumpEnhancer } from './components/CaseChoiceJumpEnhancer'
 import { V38DeadlineCardEnhancer } from './components/V38DeadlineCardEnhancer'
 import { V38AssessmentExplainability } from './components/V38AssessmentExplainability'
 import { V38PrimaryNextStep } from './components/V38PrimaryNextStep'
+import { V38MobileResilience } from './components/V38MobileResilience'
 
 export const metadata = {
   metadataBase: new URL('https://app-gold-workspace.vercel.app'),
@@ -19,5 +20,5 @@ export const metadata = {
 }
 
 export default function RootLayout({ children }) {
-  return <html lang="de"><body><HeroCopyEnhancer/><HeroTitleStabilizer/><V37FirstAction/><ProblemNavigator/><ExplainerVideo/><ProductIntroCompact/><CaseChoiceJumpEnhancer/><V38DeadlineCardEnhancer/><V38AssessmentExplainability/><V38PrimaryNextStep/>{children}</body></html>
+  return <html lang="de"><body><V38MobileResilience/><HeroCopyEnhancer/><HeroTitleStabilizer/><V37FirstAction/><ProblemNavigator/><ExplainerVideo/><ProductIntroCompact/><CaseChoiceJumpEnhancer/><V38DeadlineCardEnhancer/><V38AssessmentExplainability/><V38PrimaryNextStep/>{children}</body></html>
 }
