94ba418a392898ff6b3eee572b4dfe36f48bc4cf
sebastianauls-cmyk <sebastian.auls@gmail.com>
2026-09-04 04:54:30 +0200

    Activate V95 evidence injector across workspace views

--- app/modules/workspace/ProtectedWorkspaceShell.js
+++ app/modules/workspace/ProtectedWorkspaceShell.js
@@ -5,12 +5,14 @@ import { CountrySwitcher } from '../country/CountrySwitcher'
 import { useCountryContext } from '../country/useCountryContext'
 import { LegalFooter } from '../compliance/LegalFooter'
 import { ProductBrand } from '../brand/ProductBrand'
+import { ContextEvidenceInjector } from '../intelligence/ContextEvidenceInjector'
 
 export function ProtectedWorkspaceShell({language,outputLanguage,onLanguageChange,onOutputLanguageChange,legalLabel,languageLabel,outputLanguageLabel,logoutLabel,onLogout,message,children}){
   const {countryContext,setCountryContext}=useCountryContext()
   return <>
     <header className="appTop"><ProductBrand/><div className="appHeaderTools"><span className="legalChip">{legalLabel}</span><LanguageSwitcher value={language} onChange={onLanguageChange} label={languageLabel} showLabel/><LanguageSwitcher value={outputLanguage} onChange={onOutputLanguageChange} label={outputLanguageLabel} showLabel/><CountrySwitcher value={countryContext} onChange={setCountryContext} label="Land / Rechtsraum"/><button className="secondary" onClick={onLogout}>{logoutLabel}</button></div></header>
     <main className="appMain">{message&&<div className="note">{message}</div>}{children}</main>
+    <ContextEvidenceInjector language={language} countryCode={countryContext}/>
     <LegalFooter language={language}/>
   </>
 }
