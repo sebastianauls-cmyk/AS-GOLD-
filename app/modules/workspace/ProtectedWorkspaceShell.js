fd0730703434ff8476a9dd7494c5263fda826b2e
sebastianauls-cmyk <sebastian.auls@gmail.com>
2026-09-04 03:18:20 +0200

    Make country context self-contained in workspace shell

--- app/modules/workspace/ProtectedWorkspaceShell.js
+++ app/modules/workspace/ProtectedWorkspaceShell.js
@@ -1,11 +1,15 @@
+'use client'
+
 import { LanguageSwitcher } from '../language/LanguageSwitcher'
 import { CountrySwitcher } from '../country/CountrySwitcher'
+import { useCountryContext } from '../country/useCountryContext'
 import { LegalFooter } from '../compliance/LegalFooter'
 import { ProductBrand } from '../brand/ProductBrand'
 
-export function ProtectedWorkspaceShell({language,outputLanguage,countryContext,onLanguageChange,onOutputLanguageChange,onCountryContextChange,legalLabel,languageLabel,outputLanguageLabel,countryContextLabel='Land / Rechtsraum',logoutLabel,onLogout,message,children}){
+export function ProtectedWorkspaceShell({language,outputLanguage,onLanguageChange,onOutputLanguageChange,legalLabel,languageLabel,outputLanguageLabel,logoutLabel,onLogout,message,children}){
+  const {countryContext,setCountryContext}=useCountryContext()
   return <>
-    <header className="appTop"><ProductBrand/><div className="appHeaderTools"><span className="legalChip">{legalLabel}</span><LanguageSwitcher value={language} onChange={onLanguageChange} label={languageLabel} showLabel/><LanguageSwitcher value={outputLanguage} onChange={onOutputLanguageChange} label={outputLanguageLabel} showLabel/><CountrySwitcher value={countryContext} onChange={onCountryContextChange} label={countryContextLabel}/><button className="secondary" onClick={onLogout}>{logoutLabel}</button></div></header>
+    <header className="appTop"><ProductBrand/><div className="appHeaderTools"><span className="legalChip">{legalLabel}</span><LanguageSwitcher value={language} onChange={onLanguageChange} label={languageLabel} showLabel/><LanguageSwitcher value={outputLanguage} onChange={onOutputLanguageChange} label={outputLanguageLabel} showLabel/><CountrySwitcher value={countryContext} onChange={setCountryContext} label="Land / Rechtsraum"/><button className="secondary" onClick={onLogout}>{logoutLabel}</button></div></header>
     <main className="appMain">{message&&<div className="note">{message}</div>}{children}</main>
     <LegalFooter language={language}/>
   </>
