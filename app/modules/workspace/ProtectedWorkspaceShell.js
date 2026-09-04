5ee5ac0a48b8ae6e90e3eb7d5bcf8f98c9ad4f14
sebastianauls-cmyk <sebastian.auls@gmail.com>
2026-09-04 03:18:08 +0200

    Expose independent country context in workspace header

--- app/modules/workspace/ProtectedWorkspaceShell.js
+++ app/modules/workspace/ProtectedWorkspaceShell.js
@@ -1,10 +1,11 @@
 import { LanguageSwitcher } from '../language/LanguageSwitcher'
+import { CountrySwitcher } from '../country/CountrySwitcher'
 import { LegalFooter } from '../compliance/LegalFooter'
 import { ProductBrand } from '../brand/ProductBrand'
 
-export function ProtectedWorkspaceShell({language,outputLanguage,onLanguageChange,onOutputLanguageChange,legalLabel,languageLabel,outputLanguageLabel,logoutLabel,onLogout,message,children}){
+export function ProtectedWorkspaceShell({language,outputLanguage,countryContext,onLanguageChange,onOutputLanguageChange,onCountryContextChange,legalLabel,languageLabel,outputLanguageLabel,countryContextLabel='Land / Rechtsraum',logoutLabel,onLogout,message,children}){
   return <>
-    <header className="appTop"><ProductBrand/><div className="appHeaderTools"><span className="legalChip">{legalLabel}</span><LanguageSwitcher value={language} onChange={onLanguageChange} label={languageLabel} showLabel/><LanguageSwitcher value={outputLanguage} onChange={onOutputLanguageChange} label={outputLanguageLabel} showLabel/><button className="secondary" onClick={onLogout}>{logoutLabel}</button></div></header>
+    <header className="appTop"><ProductBrand/><div className="appHeaderTools"><span className="legalChip">{legalLabel}</span><LanguageSwitcher value={language} onChange={onLanguageChange} label={languageLabel} showLabel/><LanguageSwitcher value={outputLanguage} onChange={onOutputLanguageChange} label={outputLanguageLabel} showLabel/><CountrySwitcher value={countryContext} onChange={onCountryContextChange} label={countryContextLabel}/><button className="secondary" onClick={onLogout}>{logoutLabel}</button></div></header>
     <main className="appMain">{message&&<div className="note">{message}</div>}{children}</main>
     <LegalFooter language={language}/>
   </>
