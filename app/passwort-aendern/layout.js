d17247b5236a2b3910434d2fb9936f2e5f5fc306
sebastianauls-cmyk <sebastian.auls@gmail.com>
2026-09-08 22:51:57 +0200

    v131 prelaunch: noindex password change

--- app/passwort-aendern/layout.js
+++ app/passwort-aendern/layout.js
@@ -1,6 +1,7 @@
 export const metadata={
   title:'Passwort ändern',
-  description:'Passwort für den geschützten AS Workspace Gold Arbeitsbereich ändern.'
+  description:'Passwort für den geschützten AS Workspace Gold Arbeitsbereich ändern.',
+  robots:{index:false,follow:false}
 }
 
 export default function ChangePasswordLayout({children}){
