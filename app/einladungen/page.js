47d58e90dc52dbabded73dff60526472572c753f
sebastianauls-cmyk <sebastian.auls@gmail.com>
2026-09-09 02:43:40 +0200

    v131 promo: retire tester invitation route

--- app/einladungen/page.js
+++ app/einladungen/page.js
@@ -1,17 +1,5 @@
-import { PersonalInvitationStudio } from '../modules/marketing/PersonalInvitationStudio'
-import { InvitationWorkflowPanel } from '../modules/marketing/InvitationWorkflowPanel'
+import { redirect } from 'next/navigation'
 
-export const metadata={
-  title:'Persönliche Einladungen | AS Workspace Gold',
-  description:'Eigentümer-Modul für hochwertige persönliche Testeinladungen und den vollständigen Freigabe-Workflow.',
-  robots:{index:false,follow:false}
-}
+export const metadata={robots:{index:false,follow:false}}
 
-export default function InvitationPage(){
-  return <>
-    <PersonalInvitationStudio/>
-    <main className="wrap" style={{paddingBottom:28}}>
-      <InvitationWorkflowPanel/>
-    </main>
-  </>
-}
+export default function LegacyInvitationRoute(){redirect('/')}
